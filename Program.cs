420661dca4bcaef94f371b14c5ec5c9f710fc900
Yunus T <Yunus T@DESKTOP-F8DOEIB>
2023-01-23 00:07:05 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace elmasCiktisi
+{
+    internal class Elmas
+    {
+        private static void Main(string[] args)
+        {
+        start:
+            Console.WriteLine("bir sayı giriniz, girdiğiniz sayı elmas görüntüsünün büyüklüğünü belirleyecek :");
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= x; i++)
+            {
+                for (int y = 1; y <= x - i; y++)
+                {
+                    Console.Write(" ");
+                }
+                for (int y = 1; y <= 2 * i - 1; y++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            for (int i = x - 1; i >= 1; i--)
+            {
+                for (int y = 1; y <= x - i; y++)
+                {
+                    Console.Write(" ");
+                }
+                for (int y = 1; y <= 2 * i - 1; y++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.Write("\n\n\n");
+
+            Console.WriteLine("Başa dönmek için enter'a basınız");
+            Console.ReadLine();
+            goto start;
+        }
+    }
+}
